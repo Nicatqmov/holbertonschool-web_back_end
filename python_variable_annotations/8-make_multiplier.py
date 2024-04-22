25acaa2b2e2409e17bbc2407dc4d3514dacb78b0
GasimovNijat <nijatt.gasimov@gmail.com>
2024-04-22 14:33:32 +0400

    new file

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 from typing import Callable
+"""
+This module contains a function to multiple multipler with anoter float 
+"""
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiplier_function(x: float) -> float:

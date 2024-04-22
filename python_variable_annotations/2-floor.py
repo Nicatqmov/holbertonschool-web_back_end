7073ce068d182bfc796dee3f16621555f4131ec3
GasimovNijat <nijatt.gasimov@gmail.com>
2024-04-22 14:41:14 +0400

    new file

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-"""
-    Calculate floor of float variable
-
-"""
 import math
-def floor(n:float)->float:
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
     return math.floor(n)
+
+if __name__ == "__main__":
+    ans = floor(3.14)
+    print(ans)
